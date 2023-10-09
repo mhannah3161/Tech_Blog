fb25042d47ff1ca65a6e6433e4867d6bc4c71c25
Michael Hannah <Michaelrkhannah@gmail.com>
2023-10-09 17:51:26 -0600

    added routes and schema

---
+++ controllers/api/index.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+
+const commentRoutes = require('./comment-routes');
+const postRoutes = require('./post-routes');
+const userRoutes = require('./user-routes');
+
+
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+
+
+
+
+module.exports = router;
